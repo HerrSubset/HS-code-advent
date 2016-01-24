6999c277747d7c12ee2b0ca6e99c0326fa37d858
HerrSubset <smets.pj@gmail.com>
2016-01-24 09:45:48 +0100

    change five_leading_zeroes? functions in day 4 to make it work for puzzle 4.2

--- day4.rb
+++ day4.rb
@@ -4,7 +4,7 @@ require "digest/md5"
 ################################################################################
 # Functions
 ################################################################################
-def five_leading_zeroes?(s)
+def five_leading_zeroes?(s, six_zeroes = false)
   res = true
 
   if s[0] != "0" then res = false end
@@ -12,6 +12,9 @@ def five_leading_zeroes?(s)
   if s[2] != "0" then res = false end
   if s[3] != "0" then res = false end
   if s[4] != "0" then res = false end
+  if six_zeroes
+    if s[5] != "0" then res = false end
+  end
 
   return res
 end
@@ -27,7 +30,7 @@ loop do
   md5_input = secret_key + i.to_s
   md5 = Digest::MD5.hexdigest(md5_input)
 
-  if five_leading_zeroes?(md5)
+  if five_leading_zeroes?(md5, true)
     break
 
   else
